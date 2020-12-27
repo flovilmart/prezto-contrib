7cb552c14d450e5ac752312d1869270c44648c4a
Florent Vilmart <florent@flovilmart.com>
2020-12-27 14:57:21 -0500

    adds prompt style function for node version

--- n/init.zsh
+++ n/init.zsh
@@ -1,5 +1,5 @@
 chback_on_chdir () {
-  test -f .nvmrc && VERSION=$(cat .nvmrc) && echo "Switching to node $VERSION" && n $VERSION;
+  test -f .nvmrc && VERSION=$(cat .nvmrc) && echo "Switching to node $VERSION" && n auto;
 }
 
 # add chback_on_chdir to chpwd_functions
